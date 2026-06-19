2e4f61276c3dd5cd307a71f7b521594fb4f7cbba
Ahtesham-official <ahteshamofficial357@gmail.com>
2026-06-19 13:08:07 +0530

    Adding basic folder structure

---
+++ Server/Routes/api.route.js
@@ -0,0 +1,4 @@
+const router = require('express').router();
+
+router.post('/api/data' , (req , res) => {
+})
---
+++ Server/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.get('hello praxis');
+
+module.exports = app;
